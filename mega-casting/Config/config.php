734445dedda30ff4b5e74a092dc91485446c2c2d
SemouleDivine <f.pouet@iia-laval.fr>
2019-12-17 15:01:55 +0100

    Config test DB local

--- mega-casting/Config/config.php
+++ mega-casting/Config/config.php
@@ -6,18 +6,19 @@ class config
 
 	public static $PARAM_HOST_PROD = '';
 
-	public static $PARAM_HOST_TEST = '';
+	public static $PARAM_HOST_TEST = 'localhost';
 
 	public static $PARAM_DBNAME_PROD = '';
-	public static $PARAM_DBNAME_TEST = '';
 
-	public static $PARAM_USER_TEST = '';
+	public static $PARAM_DBNAME_TEST = 'MegaProd';
+
+	public static $PARAM_USER_TEST = 'MegaProd_User';
 
 	public static $PARAM_USER_PROD = '';
 
 	public static $PARAM_PWD_PROD = '';
 
-	public static $PARAM_PWD_TEST = '';
+	public static $PARAM_PWD_TEST = 'Not24get';
 }
 
 ?>
