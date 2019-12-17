e767aec2863edd6b0b6ef97a2b1be8977943703d
SemouleDivine <f.pouet@iia-laval.fr>
2019-12-17 14:31:02 +0100

    Classe Offre

--- mega-casting/Models/Classes/Classe.Offre.php
+++ mega-casting/Models/Classes/Classe.Offre.php
@@ -3,14 +3,19 @@
 
 class Offre
 {
-    public $Libelle;
-    public $Description;
-    public $Email;
-    public $Phone;
+    public $Identifiant;
+    public $Intitule;
+    public $Date_Debut_Publication;
+    public $Duree_Diffusion;
+    public $Date_Debut_Contrat;
+    public $Nombre_Poste;
+    public $Localisation;
+    public $Description_Poste;
+    public $Description_Profil;
 
     public function __construct()
     {
-
+      $this->Identifiant = 0
 
     }
 }
