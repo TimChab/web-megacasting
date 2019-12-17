b4e6a7776ccb3e624c8010b01a0e941f639e7a29
SemouleDivine <f.pouet@iia-laval.fr>
2019-12-17 14:23:47 +0100

    Fichiers config

---
+++ mega-casting/Config/base.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once __DIR__."/config.php";
+
+class Base
+{
+	public static function CreateConnexion()
+	{
+		$url = $_SERVER['HTTP_HOST'];
+		$length = strlen("test");
+		$lengthLocal = strlen("localhost");
+
+		if(substr($url, 0, $length) == "test" || substr($url, 0, $lengthLocal) == "localhost")
+		{
+		 	$PARAM_DBNAME = config::$PARAM_DBNAME_TEST;
+
+		 	$PARAM_HOST = config::$PARAM_HOST_TEST;
+
+		 	$PARAM_USER = config::$PARAM_USER_TEST;
+
+		 	$PARAM_PWD = config::$PARAM_PWD_TEST;
+
+		 	try
+			{
+				return new PDO("sqlsrv:server=".$PARAM_HOST.";database=".$PARAM_DBNAME."", $PARAM_USER, $PARAM_PWD);
+			}
+
+			catch ( PDOException $e )
+			{
+				echo "<h1>Problème de connexion a la base de données</h1>".$e;
+			}
+		}
+
+		else
+		{
+			$PARAM_DBNAME = config::$PARAM_DBNAME_PROD;
+			$PARAM_HOST = config::$PARAM_HOST_PROD;
+		 	$PARAM_USER = config::$PARAM_USER_PROD;
+		 	$PARAM_PWD = config::$PARAM_PWD_PROD;
+
+
+			// creation de la connexion afin d'executer les requetes
+			try
+			{
+				return new PDO("sqlsrv:server=".$PARAM_HOST.";database=".$PARAM_DBNAME."", $PARAM_USER, $PARAM_PWD);
+			}
+
+			catch ( PDOException $e )
+			{
+				echo "<h1>Problème de connexion a la base de données</h1>".$e;
+			}
+		}
+	}
+}
+
+?>
---
+++ mega-casting/Config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+class config
+{
+	//BDD
+
+	public static $PARAM_HOST_PROD = 'plesk1.rockett.fr';
+
+	public static $PARAM_HOST_TEST = 'plesk1.rockett.fr';
+
+	public static $PARAM_DBNAME_PROD = 'PGal_Prod_DB';
+
+	public static $PARAM_DBNAME_TEST = 'PGal_Test_DB';
+
+	public static $PARAM_USER_TEST = 'PGal_Test_User';
+
+	public static $PARAM_USER_PROD = 'PGal_Prod_User';
+
+	public static $PARAM_PWD_PROD = '7ebM8#v5';
+
+	public static $PARAM_PWD_TEST = '0p8I_7ww';
+}
+
+?>
