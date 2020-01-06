829f07d6f1be35cef971f35237c10edf7e7ef834
SemouleDivine <f.pouet@iia-laval.fr>
2020-01-06 12:03:20 +0100

    Barre de recherche

--- index.php
+++ index.php
@@ -38,6 +38,13 @@ $AnnonceList = OffreRepository::GetAllOffres();
 			<!-- Main -->
 				<div id="main">
 					<div class="inner">
+						<form class="form" action="index.html" method="post">
+							<div class="form-group search-bar">
+								<input type="text" placeholder="Rechercher une offre"/>
+								<input type="submit" value="Rechercher"/>
+							</div>
+
+						</form>
 
 					<!-- Boxes -->
 						<div class="thumbnails">
@@ -46,7 +53,7 @@ $AnnonceList = OffreRepository::GetAllOffres();
 									foreach ($AnnonceList as $key => $Annonce) {
 										echo '
 										<div class="box">
-											<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="assets/images/pic01.jpg" alt="" /></a>
+											<div class="image fit"><img src="assets/images/pic01.jpg" alt="" /></div>
 											<div class="inner">
 												<h3>'. $Annonce->Intitule .'</h3>
 												<p>'.$Annonce->Description_Poste.'</p>
@@ -57,53 +64,6 @@ $AnnonceList = OffreRepository::GetAllOffres();
 										';
 									}
 							?>
-
-<!--
-
-							<div class="box">
-								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="assets/images/pic02.jpg" alt="" /></a>
-								<div class="inner">
-									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button style2 fit" data-poptrox="youtube,800x400">Watch</a>
-								</div>
-							</div>
-
-							<div class="box">
-								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="assets/images/pic03.jpg" alt="" /></a>
-								<div class="inner">
-									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button style3 fit" data-poptrox="youtube,800x400">Watch</a>
-								</div>
-							</div>
-
-							<div class="box">
-								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="assets/images/pic04.jpg" alt="" /></a>
-								<div class="inner">
-									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button style2 fit" data-poptrox="youtube,800x400">Watch</a>
-								</div>
-							</div>
-
-							<div class="box">
-								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="assets/images/pic05.jpg" alt="" /></a>
-								<div class="inner">
-									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button style3 fit" data-poptrox="youtube,800x400">Watch</a>
-								</div>
-							</div>
-
-							<div class="box">
-								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="assets/images/pic06.jpg" alt="" /></a>
-								<div class="inner">
-									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button fit" data-poptrox="youtube,800x400">Watch</a>
-								</div>
-							</div> -->
 
 						</div>
 
@@ -136,3 +96,10 @@ $AnnonceList = OffreRepository::GetAllOffres();
 
 	</body>
 </html>
+<style media="screen">
+	.search-bar {
+		display: inline-flex;
+		position: absolute;
+		right: 1%;
+	}
+</style>
